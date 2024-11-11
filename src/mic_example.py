cc0ee321a895ab7d1d08e7b55d67dba595f5ac36
Darien Martinez Torres <darien.martinez@gmail.com>
2024-11-11 02:26:49 -0500

    Using openai whisper local

--- src/mic_example.py
+++ src/mic_example.py
@@ -5,14 +5,12 @@ import time
 def callback(recognizer, audio):
     # received audio data, now we'll recognize it using Google Speech Recognition
     try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        print("Google Speech Recognition thinks you said: " + recognizer.recognize_google(audio))
+        text = recognizer.recognize_whisper(audio)
+        print("Said: " + text)
     except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
+        print("Could not understand audio")
     except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        print("Could not get results from API; {0}".format(e))
 
 def main():
     # for index, name in enumerate(sr.Microphone.list_microphone_names()):
