45d524c3a3f796af5b5bf80e3e566272d64225aa
Darien Martinez Torres <darien.martinez@gmail.com>
2024-11-16 02:04:23 -0500

    Main threads update

--- src/main.py
+++ src/main.py
@@ -1,52 +1,78 @@
 import argparse
+import pyaudio
+from queue import Queue
+from threading import Thread
+from datetime import datetime
 
 import speech_recognition as sr
-import time
+
+from select_input import select_device_index
+
+def listening(recognizer, selected_device_index, control, bus, output):
+    m = sr.Microphone(device_index=selected_device_index, sample_rate=16000)
+    with m as source:
+        recognizer.adjust_for_ambient_noise(source)
+        output.put("Listening...")
+        while not control.empty():
+            bus.put( recognizer.listen(source) )
+
+def outputWriter(control, output):
+    while not control.empty():
+        print(f"[{datetime.now()}] : {output.get()}")
+
+def toText(recognizer, control, bus, output):
+    while not control.empty():
+        try:
+            text = recognizer.recognize_whisper(bus.get())
+            output.put(text)
+        except sr.UnknownValueError:
+            output.put("Could not understand audio")
+        except sr.RequestError as e:
+            output.put("Could not get results from API; {0}".format(e))
 
 def start_processing(device_index):
-    print(f"Device index: {device_index}")
+    messages = Queue()
+    recordings = Queue()
+    output = Queue()
 
-def select_device_index():
-    devices = sr.Microphone.list_microphone_names()
-    device_index = None
-    while device_index is None:
-        print("Select Device:")
-        for index, name in enumerate(devices):
-            print(f"Index: {index} - {name}")
-        device = input("Enter device index: ")
-        try:
-            device_index = int(device)
-        except ValueError:
-            print("Invalid input. Please enter an integer")
-            continue
-        
-        if device_index >= len(devices):
-            print("Please enter one of the shown indexes")
-            device_index = None
-    return device_index
+    messages.put(True)
+
+    r = sr.Recognizer()
+    listener = Thread(target=listening, args=(r, device_index, messages, recordings, output,), daemon=True)
+    listener.start()
+
+    converter = Thread(target=toText, args=(r, messages, recordings, output,), daemon=True)
+    converter.start()
+
+    writer = Thread(target=outputWriter, args=(messages,output,), daemon=True)
+    writer.start()
+    
+    input()
+    messages.get()
+
+    listener.join()
+    converter.join()
+    writer.join()
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--device_index", help="Device index")
+    parser.add_argument("--device-index", help="Device index")
     args = parser.parse_args()
     device_index = args.device_index
     if device_index is None:
-        device_index = select_device_index()        
+
+        p = pyaudio.PyAudio()
+        print()
+        try:
+            device_info = select_device_index(p)
+            if device_info is None:
+                return
+            device_index = device_info["index"]  
+        finally:
+            p.terminate()
 
     start_processing(device_index=device_index)
-    
-    
-
-    
-    # r = sr.Recognizer()
-    # m = sr.Microphone(device_index=4)
-
-
-    # stop_listening = r.listen_in_background(m, callback)
-
-    # while True:
-        # time.sleep(0.1)
-
 
 if __name__ == "__main__":
     main()
